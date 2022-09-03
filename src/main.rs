63c8b91ea7d123912c6c957138ca19e9ec47882f
Praxis <hello@vulpinecat.com>
2022-09-03 16:07:39 +0200

    rename factory and middleware

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,10 @@ use actix_web::{
 };
 use futures_util::future::LocalBoxFuture;
 
-// There are two steps in middleware processing.
-// 1. Middleware initialization, middleware factory gets called with
-//    next service in chain as parameter.
-// 2. Middleware's call method gets called with normal request.
-pub struct SayHi;
 
-// Middleware factory is `Transform` trait
-// `S` - type of the next service
-// `B` - type of response's body
-impl<S, B> Transform<S, ServiceRequest> for SayHi
+pub struct Ed25519Authenticator;
+
+impl<S, B> Transform<S, ServiceRequest> for Ed25519Authenticator
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
     S::Future: 'static,
@@ -24,19 +18,19 @@ where
     type Response = ServiceResponse<B>;
     type Error = Error;
     type InitError = ();
-    type Transform = SayHiMiddleware<S>;
+    type Transform = Ed25519AuthenticatorMiddleware<S>;
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
-        ready(Ok(SayHiMiddleware { service }))
+        ready(Ok(Ed25519AuthenticatorMiddleware { service }))
     }
 }
 
-pub struct SayHiMiddleware<S> {
+pub struct Ed25519AuthenticatorMiddleware<S> {
     service: S,
 }
 
-impl<S, B> Service<ServiceRequest> for SayHiMiddleware<S>
+impl<S, B> Service<ServiceRequest> for Ed25519AuthenticatorMiddleware<S>
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
     S::Future: 'static,
